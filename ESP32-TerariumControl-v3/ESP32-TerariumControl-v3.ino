ebcd6f7df7dce8b630f014655d7067078b6e2dfe
Peter Dalmaris <peter@futureshock.com.au>
2020-08-25 14:34:41 +1000

    Added new header

--- ESP32-TerariumControl-v3/ESP32-TerariumControl-v3.ino
+++ ESP32-TerariumControl-v3/ESP32-TerariumControl-v3.ino
@@ -1,3 +1,18 @@
+/*  Node-RED and ESP32 Make a Terrarium Controller
+ * 
+ * This code is part of a course from Tech Explorations.
+ * For information about this course, please see
+ * 
+ * https://techexplorations.com/so/nodered/
+ * 
+ * For information on hardware components and the wiring needed to 
+ * run this sketch, please see the relevant lecture in the course.
+ * 
+ *  
+ *  Created by Peter Dalmaris
+ * 
+ */
+
 // This version makes it possible to set the pump activation threshold in the dashboard.
 
 #include "EspMQTTClient.h"
