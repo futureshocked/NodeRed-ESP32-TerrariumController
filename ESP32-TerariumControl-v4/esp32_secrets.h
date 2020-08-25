f46105661ae86ab33dbfd8a914113652665c5850
Peter Dalmaris <peter@futureshock.com.au>
2020-08-25 14:35:39 +1000

    Added new header

--- ESP32-TerariumControl-v4/esp32_secrets.h
+++ ESP32-TerariumControl-v4/esp32_secrets.h
@@ -1,3 +1,19 @@
+/*  Node-RED and ESP32 Make a Terrarium Controller
+ * 
+ * This code is part of a course from Tech Explorations.
+ * For information about this course, please see
+ * 
+ * https://techexplorations.com/so/nodered/
+ * 
+ * For information on hardware components and the wiring needed to 
+ * run this sketch, please see the relevant lecture in the course.
+ * 
+ *  
+ *  Created by Peter Dalmaris
+ * 
+ */
+
+
 #define SECRET_SSID  "<your_ssid>"
 #define SECRET_PASS  "<your_password>"
 #define BROKER_USERNAME  "<your_broker_client_username>"
