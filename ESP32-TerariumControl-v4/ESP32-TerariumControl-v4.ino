b24143d69bc757adaf5c3d7af355a7088481f28a
Peter Dalmaris <peter@futureshock.com.au>
2020-08-25 14:35:21 +1000

    Added new header

--- ESP32-TerariumControl-v4/ESP32-TerariumControl-v4.ino
+++ ESP32-TerariumControl-v4/ESP32-TerariumControl-v4.ino
@@ -1,3 +1,19 @@
+/*  Node-RED and ESP32 Make a Terrarium Controller
+ * 
+ * This code is part of a course from Tech Explorations.
+ * For information about this course, please see
+ * 
+ * https://techexplorations.com/so/nodered/
+ * 
+ * For information on hardware components and the wiring needed to 
+ * run this sketch, please see the relevant lecture in the course.
+ * 
+ *  
+ *  Created by Peter Dalmaris
+ * 
+ */
+
+
 // This version introduced the Task scheduler and the pump safety cuttoff.
 
 #include "EspMQTTClient.h"
