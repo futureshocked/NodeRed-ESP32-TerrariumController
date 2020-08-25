765a428111d886d400ebb394bcb7b4c2cca1ff42
Peter Dalmaris <peter@futureshock.com.au>
2020-08-25 14:36:01 +1000

    Added new header

--- ESP32-TerariumControl-v5/ESP32-TerariumControl-v5.ino
+++ ESP32-TerariumControl-v5/ESP32-TerariumControl-v5.ino
@@ -1,3 +1,19 @@
+/*  Node-RED and ESP32 Make a Terrarium Controller
+ * 
+ * This code is part of a course from Tech Explorations.
+ * For information about this course, please see
+ * 
+ * https://techexplorations.com/so/nodered/
+ * 
+ * For information on hardware components and the wiring needed to 
+ * run this sketch, please see the relevant lecture in the course.
+ * 
+ *  
+ *  Created by Peter Dalmaris
+ * 
+ */
+
+
 // This version introduces the pump safety cuttoff.
 // The pump will stop working after 5 seconds to ensure that the
 // terrarium will not flood in case of loss of communication.
